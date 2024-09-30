983403042dead1cebf85bca0b2bebdc44be91b2b
leonardo <leonardo.a.medina@gmail.com>
2024-09-30 18:59:42 -0300

    fix header and footer script, resize improvements

--- scripts/includeHeaderFooter.js
+++ scripts/includeHeaderFooter.js
@@ -1,8 +1,9 @@
-async function loadHTML(file, elementId) {
+async function loadHTML(file, tagName) {
     const response = await fetch(file);
     const text = await response.text();
-    document.getElementById(elementId).innerHTML = text;
+    document.getElementsByTagName(tagName)[0].innerHTML = text;
 }
 
 loadHTML('commons/header.html', 'header');
 loadHTML('commons/footer.html', 'footer');
+
