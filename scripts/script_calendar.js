f749e1fcf15c1d4375ae774a76b6859e7c576957
josunlam <josunlam@gmail.com>
2024-10-01 10:56:42 -0300

    modificaciones 11am

--- scripts/script_calendar.js
+++ scripts/script_calendar.js
@@ -18,19 +18,30 @@ function cargar_calendario()
     arr_articles[42] = "";
     
     var fec= document.getElementById('date').value+'-1';
+    var fec_aux=document.getElementById('date').value+'-1';
     var dt=new Date(fec);
-    
+
+    var mes=fec_aux.split("-");
+    mes=mes[1];
+ 
     var dia_sem;
+    var aux_mes;
     for (let i = 0; i < arr_articles.length; i++) {
         var aux=dt.getUTCDay();
         var aux2=dias[dt.getUTCDay()];
         var aux3=arr_dias[dt.getUTCDay()][1];
         if(i==0)
             dt.setDate(dt.getDate()-aux3);
+
         dia_sem=dt.getDate();
-        
         document.getElementById('id_'+i).innerHTML=dia_sem;
+
+        aux_mes=dt.getMonth()+1;
+
+        if(mes!=aux_mes)
+            document.getElementById('calendar_id_'+i).style.backgroundColor="transparent";
+        else
+            document.getElementById('calendar_id_'+i).style.backgroundColor="white";
         dt.setDate(dt.getDate()+1);
-        debugger;
     }
 }
